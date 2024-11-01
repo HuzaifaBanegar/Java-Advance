f18263067126b7bcf0ece6dca1d4bebe4e580672
Huzaifa Banegar <141048867+BanegarHuzaifa@users.noreply.github.com>
2024-11-01 14:37:35 +0530

    Abstract classes minor change

--- AdvanceJavaLearning/src/AbstraxtClasses/Car.java
+++ AdvanceJavaLearning/src/AbstraxtClasses/Car.java
@@ -2,6 +2,9 @@ package AbstraxtClasses;
 
 public class Car extends  Vehicle{
 
+    public Car(){
+        System.out.println("Car is constrcted");
+    }
     void startEngine(){
         System.out.println("Car Engine started");
     }
--- AdvanceJavaLearning/src/AbstraxtClasses/Vehicle.java
+++ AdvanceJavaLearning/src/AbstraxtClasses/Vehicle.java
@@ -4,6 +4,17 @@ abstract public class Vehicle {
     //For all the abstract methods it is necessary to provide implementation in the child class when created
     abstract void startEngine();
 
+    // Abstract class can have its own members
+    private boolean created;
+
+    //Abstract method can have constrctor
+    public Vehicle(){
+        System.out.println("A vehicle is being constructed");
+        created = true;
+        System.out.println("Is Vehicle constructed? "+ created);
+    }
+
+    //It can also have complete methods
     void stopEngine(){
         System.out.println("Engine stopped");
     }
